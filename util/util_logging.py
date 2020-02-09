4c0f860e099d897a04fa3a4e92411112cc93aace
Barrie <BarrieCAU@outlook.com>
2020-02-09 12:24:08 +0800

    Update the structure of utils
    Split into three sub-files
    Implement functions with classes

---
+++ util/util_logging.py
@@ -0,0 +1,75 @@
+import os,sys
+import datetime
+import json
+import logging
+
+from util_parameter import *
+
+
+class util_logging(util_parameter):
+
+    def __init__(self):
+
+        super().__init__()
+
+        present_time = datetime.datetime.now()
+        self.log_name = datetime.datetime.strftime(present_time, '%Y-%m-%d %H-%M-%S')
+        # 用当前时间为本次日志命名
+
+        self.logger = logging.getLogger(__name__)
+        self.file_handler = logging.FileHandler(self.get_fullurl("log", self.log_name, "txt"))
+        self.stream_handler = logging.StreamHandler()
+        self.formatter = logging.Formatter('%(asctime)s - %(filename)s - %(funcName)s - %(levelname)s - %(message)s')
+        # file_handler: 文件输出
+        # stream_handler: 数据流输出
+        # formatter: 统一的日志输出格式
+
+    def log_init(self):
+        # 初始化日志文件，并写入参数表
+
+        content = []
+        for part_name in self.part:
+            content.append(part_name)
+            for k in self.config[part_name]:
+                content.append("|---" + k + ':')
+                content.append("    |---type: " + str(self.config_menu[part_name][k]["type"]))
+                content.append("    |---description: " + str(self.config_menu[part_name][k]["description"]))
+                content.append("    |---value: " + str(self.config[part_name][k]))
+            content.append('')
+        # 输出全部参数信息
+        jsonstr = json.dumps(self.config)
+        content.append(jsonstr)
+        # 输出可进行使用的json格式参数配置
+        content.append('\n\n')
+        self.save_file(content, "log", self.log_name, 'txt')
+
+        self.logger.setLevel(level=logging.INFO)
+        self.file_handler.setLevel(logging.INFO)
+        self.file_handler.setFormatter(self.formatter)
+        self.logger.addHandler(self.file_handler)
+        self.stream_handler.setLevel(logging.INFO)
+        self.stream_handler.setFormatter(self.formatter)
+        self.logger.addHandler(self.stream_handler)
+
+    def log_input(self, level, message):
+        # 记录日志信息，会同步输出到命令行和日志文件中
+        # level为'debug', 'info', 'warning', 'error', 'critical'，分别用数值1-5表示
+
+        if level == 1:
+            self.logger.debug(message)
+        elif level == 2:
+            self.logger.info(message)
+        elif level == 3:
+            self.logger.warning(message)
+        elif level == 4:
+            self.logger.error(message)
+        elif level == 5:
+            self.logger.critical(message)
+
+
+"""
+util = util_logging()
+util.read_config_default()
+util.log_init()
+util.log_input(5, "success!")
+"""
---
+++ util/util_parameter.py
@@ -0,0 +1,81 @@
+import os,sys
+import getopt
+
+from util_filepath import *
+
+
+class util_parameter(util_filepath):
+
+    def __init__(self):
+
+        super().__init__()
+
+        self.config_menu = {}
+        self.config = {}
+        self.part = ["global", "build", "train", "predict"]
+        for part_name in self.part:
+            self.config[part_name] = {}
+        # config_menu: 变量目录
+        # config: 变量值
+        # part: 模块划分
+
+    def read_config_default(self, file_name="config_default", file_format="json"):
+        # 读入变量定义文件，建立变量目录，并填充默认值
+        file_type = "config"
+
+        self.config_menu = self.read_file(file_type, file_name, file_format)[0]
+        for part_name in self.part:
+            self.config[part_name] = {}
+        for part_name in self.config_menu.keys():
+            try:
+                for k in self.config_menu[part_name].keys():
+                    self.config[part_name][k] = self.config_menu[part_name][k]["default"]
+            except KeyError:
+                return
+                # 出现未知模块名，返回错误信息
+
+    def read_config_file(self, file_name, file_format="json"):
+        # 读入变量设置文件，管理新定义的变量，并读入其他设置值
+        file_type = "config"
+
+        config_new = self.read_file(file_type, file_name, file_format)[0]
+        for part_name in config_new.keys():
+            try:
+                for k in config_new[part_name].keys():
+                    if k not in self.config_menu[part_name].keys():
+                        self.config_menu[part_name][k] = config_new[part_name][k]
+                        self.config[part_name][k] = self.config_menu[part_name][k]["default"]
+                    else:
+                        self.config[part_name][k] = config_new[part_name][k]
+            except KeyError:
+                return
+                # 出现未知模块名，返回错误信息
+
+    def set_config(self, argv):
+        # 录入从命令行获取的参数值，只处理长格式
+        # set_config(sys.argv[1:])
+
+        config_list = []
+        for part_name in self.part:
+            for k in self.config_menu[part_name].keys():
+                config_list.append(k + '=')
+        try:
+            options, args = getopt.getopt(argv, "", config_list)
+        except getopt.GetoptError:
+            sys.exit()
+            # argv格式错误，返回错误信息
+
+        for option, value in options:
+            for part_name in self.part:
+                for k in self.config_menu[part_name].keys():
+                    if option == '--' + k:
+                        self.config[part_name][k] = value
+
+    def get_config(self, part_name):
+        # 获取全局变量与对应部分的函数值
+
+        try:
+            return self.config["global"], self.config[part_name]
+        except KeyError:
+            return {}, {}
+            # 出现未知模块名，返回错误信息

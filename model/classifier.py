430eae383a1d146e48663ec807b8edc55ba69bad
GuoZhilong <39120200+GuoZhilong@users.noreply.github.com>
2020-02-10 15:35:38 +0800

    Add files via upload

--- model/classifier.py
+++ model/classifier.py
@@ -68,7 +68,7 @@ def create_model(args,
         logits=logits, label=labels, return_softmax=True)
     loss = fluid.layers.mean(x=ce_loss)
 
-    if args.use_fp16 and args.loss_scaling > 1.0:
+    if args['use_fp16'] and args.loss_scaling > 1.0:
         loss *= args.loss_scaling
 
     num_seqs = fluid.layers.create_tensor(dtype='int64')

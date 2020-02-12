4b4241eca22167de766dc5cb7d041d95cd8f2cbb
Barrie <BarrieCAU@outlook.com>
2020-02-12 19:32:08 +0800

    PreProcess.get_vocab_size()

--- preprocess/preprocess.py
+++ preprocess/preprocess.py
@@ -45,6 +45,13 @@ class PreProcess:
 
         self.features = []
 
+    def get_vocab_size(self):
+        """
+        获取使用的词表的大小
+        """
+
+        return len(self.c_token.vocab)
+
     def get_examples_from_file(self, file_name, file_format="pickle", file_type="example"):
         """
         读取指定文件，返回list of Example结果
